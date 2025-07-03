0fb9b892c70cfd2700c86a78a0db4dc6d2928008
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2025-07-03 17:29:29 +1000

    Added comments with objectives.

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
- 
+//Name: Eduardo Biscaia de Queiroz
+//Date: 03/07/2025
+
+//Objectives:
+//Create a program that calculates the prime numbers under a given number
+//Factorize the number, giving all the prime divisors, prime divisors and
+//their repetition or all the divisors of a given number using golang flags
+
+//Examples:
+//--all-primes 50
+//[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47]
+
+//--factors 50
+//[2,5,5]
+
+//--factor-pairs 50
+//[(2,1),(5,2)]
+
+//--divisors 50
+//[1,2.5,10,50]
+
